112e02d7ae69b7027da4a7997f36f9ac37db208b
minhct13 <congminht91@gmail.com>
2023-03-05 01:32:40 +0700

    init flask app and database

---
+++ backend/app/__init__.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from config import config
+from flask_cors import CORS
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config.from_object(config)
+
+    api_prefix = app.config["APP_API_PREFIX"]
+        
+    CORS(
+          app, resources={
+            rf"{api_prefix}/*": {
+              "origins": [
+                "*"
+              ],
+              "supports_credentials": True,
+            }
+          }
+        )
+    # Import a module / component using its blueprint handler variable
+    # app.register_blueprint(query_bp, url_prefix=api_prefix)
+    # app.register_blueprint(feature_bp, url_prefix=api_prefix)
+    
+    return app
