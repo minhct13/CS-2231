c852b977496ada5105b8f7e6bce0289b9d9a5769
minhct13 <congminht91@gmail.com>
2023-03-05 22:40:22 +0700

    update

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,14 +1,26 @@
 from flask import Flask
 from config import config
 from flask_cors import CORS
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from app.models import db
+
+
+migrate = Migrate()
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-
     app.config.from_object(config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     api_prefix = app.config["APP_API_PREFIX"]
         
+
+
     CORS(
           app, resources={
             rf"{api_prefix}/*": {
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -10,4 +10,9 @@ def convert_list_object_from_string(string):
 class Config():
     APP_API_PREFIX = environ.get("APP_API_PREFIX")
     SECRET_KEY = environ.get("SECRET_KEY")
-    DATABASE_URL = environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI  = environ.get("DATABASE_URL")
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    POSTGRES_USER = environ.get("POSTGRES_USER")
+    POSTGRES_PASSWORD = environ.get("POSTGRES_PASSWORD")
+    POSTGRES_DB = environ.get("POSTGRES_DB")
+    POSTGRES_NAME = environ.get("POSTGRES_NAME")
